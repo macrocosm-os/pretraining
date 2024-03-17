bf99254b672b31b3d4a7d49cd96efeaadcf1b497
Sid <sid.data.universe@gmail.com>
2024-03-16 18:49:28 -0700

    Fix mistral typo.

--- scripts/run_benchmarks.py
+++ scripts/run_benchmarks.py
@@ -267,17 +267,17 @@ def run_benchmarks(args: ArgumentParser, datasets: Dict[str, str]):
     # )
     models = {
         # # best_model_hf: best_model_provider,
-        "gpt2": HuggingFaceModelProvider("gpt2", args.cache_dir),
-        "gpt2-large": HuggingFaceModelProvider("gpt2-large", args.cache_dir),
-        # # Also run a 1.5b for comparison.
-        "gpt2-xl": HuggingFaceModelProvider("gpt2-xl", args.cache_dir),
-        # Also run a 3b for comparison.
-        "phi-2": HuggingFaceModelProvider("microsoft/phi-2", args.cache_dir),
-        # Run Falcon 7b to start
+        # "gpt2": HuggingFaceModelProvider("gpt2", args.cache_dir),
+        # "gpt2-large": HuggingFaceModelProvider("gpt2-large", args.cache_dir),
+        # # # Also run a 1.5b for comparison.
+        # "gpt2-xl": HuggingFaceModelProvider("gpt2-xl", args.cache_dir),
+        # # Also run a 3b for comparison.
+        # "phi-2": HuggingFaceModelProvider("microsoft/phi-2", args.cache_dir),
+        # # Run Falcon 7b to start
         "falcon-7b": HuggingFaceModelProvider("tiiuae/falcon-7b", args.cache_dir),
         # Add Mistral and gemma after for comparison.
         "Mistral-7B-v0.1 ": HuggingFaceModelProvider(
-            "mistralai/Mistral-7B-v0.1 ", args.cache_dir
+            "mistralai/Mistral-7B-v0.1", args.cache_dir
         ),
         "gemma-7b": HuggingFaceModelProvider("google/gemma-7b", args.cache_dir),
     }
