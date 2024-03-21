6dc3d6c80a2cf071f29e383e267ac2cb4f1175b3
Sid <sid.data.universe@gmail.com>
2024-03-20 19:50:48 -0700

    Gemma after login.

--- scripts/run_benchmarks.py
+++ scripts/run_benchmarks.py
@@ -279,13 +279,13 @@ def run_benchmarks(args: ArgumentParser, datasets: Dict[str, str]):
         # NOTE: running 7b models doesn't seem to work in parallel. Need to restart process between.
         # Otherwise they run into memory issues. Not sure if the del model handles that properly.
         # # Run Falcon 7b to start
-        "falcon-7b": HuggingFaceModelProvider("tiiuae/falcon-7b", args.cache_dir),
+        # "falcon-7b": HuggingFaceModelProvider("tiiuae/falcon-7b", args.cache_dir),
         # Add Mistral and gemma after for comparison.
-        "Mistral-7B-v0.1 ": HuggingFaceModelProvider(
-            "mistralai/Mistral-7B-v0.1", args.cache_dir
-        ),
+        # "Mistral-7B-v0.1 ": HuggingFaceModelProvider(
+        #    "mistralai/Mistral-7B-v0.1", args.cache_dir
+        # ),
         # Gemma is gated, need to share contact info and such.
-        # "gemma-7b": HuggingFaceModelProvider("google/gemma-7b", args.cache_dir),
+        "gemma-7b": HuggingFaceModelProvider("google/gemma-7b", args.cache_dir),
     }
 
     ppls = defaultdict(list)
