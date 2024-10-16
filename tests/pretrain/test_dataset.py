d12e8abf0a4a7765074c63b03750dc87746768f8
Sid <sid.data.universe@gmail.com>
2024-10-15 20:42:12 -0700

    Ensure fineweb loader chooses page starts offset by rows per page.

--- tests/pretrain/test_dataset.py
+++ tests/pretrain/test_dataset.py
@@ -11,38 +11,79 @@ from neurons import config
 config = config.validator_config()
 
 
-def test_FineWeb_loader_page_copy():
-    """
-    Test that pages can be correctly copied from one FineWeb dataloader
-    to another
-    """
-    # Some test params
-    NUM_PAGES = 20
+class TestDataset(unittest.TestCase):
+    def test_fineweb_loader_page_copy(self):
+        """
+        Test that pages can be correctly copied from one FineWeb dataloader
+        to another
+        """
+        # Some test params
+        NUM_PAGES = 20
 
-    # Load a tokenizer
-    tokenizer = pt.model.load_tokenizer(
-        MODEL_CONSTRAINTS_BY_COMPETITION_ID[CompetitionId.B7_MODEL],
-        cache_dir=config.model_dir,
-    )
+        # Load a tokenizer
+        tokenizer = pt.model.load_tokenizer(
+            MODEL_CONSTRAINTS_BY_COMPETITION_ID[CompetitionId.B7_MODEL],
+            cache_dir=config.model_dir,
+        )
 
-    # First dataloader
-    dataloader_1 = pt.dataset.SubsetFineWebEdu2Loader(
-        batch_size=4, sequence_length=4092, num_pages=NUM_PAGES, tokenizer=tokenizer
-    )
+        # First dataloader
+        dataloader_1 = pt.dataset.SubsetFineWebEdu2Loader(
+            batch_size=4, sequence_length=4092, num_pages=NUM_PAGES, tokenizer=tokenizer
+        )
 
-    # Assert that the number of pages loaded successfully are the one required
-    assert len(dataloader_1.pages) == NUM_PAGES
+        # Assert that the number of pages loaded successfully are the one required
+        assert len(dataloader_1.pages) == NUM_PAGES
 
-    # Now create a second loader without automatic page loading
-    dataloader_2 = pt.dataset.SubsetFineWebEdu2Loader(
-        batch_size=4, sequence_length=4092, num_pages=None, tokenizer=tokenizer
-    )
+        # Now create a second loader without automatic page loading
+        dataloader_2 = pt.dataset.SubsetFineWebEdu2Loader(
+            batch_size=4, sequence_length=4092, num_pages=None, tokenizer=tokenizer
+        )
 
-    # Copy pages from the first dataloader
-    dataloader_2.fetch_data_for_pages(pages=dataloader_1.pages)
+        # Copy pages from the first dataloader
+        dataloader_2.fetch_data_for_pages(pages=dataloader_1.pages)
 
-    # Assert both dataloaders have the same pages
-    assert set(dataloader_1.pages) == set(dataloader_2.pages)
+        # Assert both dataloaders have the same pages
+        assert set(dataloader_1.pages) == set(dataloader_2.pages)
 
-    # Assert that both have the same buffers
-    assert np.array_equal(dataloader_1.buffer, dataloader_2.buffer)
+        # Assert that both have the same buffers
+        assert np.array_equal(dataloader_1.buffer, dataloader_2.buffer)
+
+    def test_fineweb_loader_page_offset(self):
+        """Tests that the fineweb loader will only generate page starts that are num rows per pages apart."""
+        # Load a tokenizer
+        tokenizer = pt.model.load_tokenizer(
+            MODEL_CONSTRAINTS_BY_COMPETITION_ID[CompetitionId.B7_MODEL],
+            cache_dir=config.model_dir,
+        )
+
+        dataloader = pt.dataset.SubsetFineWebEdu2Loader(
+            batch_size=0, sequence_length=4092, num_pages=0, tokenizer=tokenizer
+        )
+
+        # Ensure we know the num_rows_per_page.
+        test_num_rows_per_page = 100
+        dataloader.num_rows_per_page = test_num_rows_per_page
+
+        # Create a fake configs data with only 599 rows.
+        dataloader.configs_data = {
+            "CC-MAIN-2013-20": {"num_rows": 599, "split": "train"}
+        }
+
+        # Ensure get random pages returns only 0, 100, 200, 300, 400 and 500
+        expected_page_starts = {0, 100, 200, 300, 400, 500}
+        for _ in range(1000):
+            random_pages = dataloader.get_random_pages(1)
+            _, page_start, _ = random_pages[0]
+            self.assertTrue(page_start in expected_page_starts)
+
+        # Create a fake configs data with only 598 rows.
+        dataloader.configs_data = {
+            "CC-MAIN-2013-20": {"num_rows": 598, "split": "train"}
+        }
+
+        # Ensure get random pages returns only 0, 100, 200, 300, and 400 (since 500-598 is not 100 rows)
+        expected_page_starts = {0, 100, 200, 300, 400}
+        for _ in range(1000):
+            random_pages = dataloader.get_random_pages(1)
+            _, page_start, _ = random_pages[0]
+            self.assertTrue(page_start in expected_page_starts)
