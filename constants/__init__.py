170049897f3d2518865ebd1cc2a737c59d845c5b
cryptal-mc <alan.aboudib@macrocosmos.ai>
2024-12-03 16:16:13 +0000

    Bumped validator version

--- constants/__init__.py
+++ constants/__init__.py
@@ -37,7 +37,7 @@ from typing import Dict, List, Tuple
 __version__ = "4.6.2"
 
 # Validator schema version
-__validator_version__ = "4.6.0"
+__validator_version__ = "4.6.2"
 version_split = __validator_version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
